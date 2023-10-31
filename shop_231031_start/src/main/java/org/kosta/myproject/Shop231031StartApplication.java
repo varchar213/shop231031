a390796473e75695e2618b59787ed3072fbc0670
varchar213 <jmcs7eg1@gmail.com>
2023-10-31 16:13:03 +0900

    git 초기설정 연동
    git 초기설정 연동

---
+++ shop_231031_start/src/main/java/org/kosta/myproject/Shop231031StartApplication.java
@@ -0,0 +1,13 @@
+package org.kosta.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop231031StartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop231031StartApplication.class, args);
+	}
+
+}
---
+++ shop_231031_start/src/main/java/org/kosta/myproject/controller/homeController.java
@@ -0,0 +1,15 @@
+package org.kosta.myproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeController {
+
+	
+		@RequestMapping("/")
+		public String home() {
+			
+			return "index";
+		}
+}
